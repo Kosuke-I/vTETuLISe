2326f76203100a06048bbd61e81c7bcfa4fb7f0a
Kosuke-I <ki.program0915@gmail.com>
2020-04-26 13:52:21 +0900

    refs: フィールド画面作成 html大枠の作成

---
+++ pages/vTETuLISe.vue
@@ -0,0 +1,321 @@
+<template>
+  <div id="app">
+    <div class="display">
+      <div class="field">
+        <div class="left">
+          <div class="queue holdQueue">
+            hold
+          </div>
+          <div class="gameInformation">
+            <div class="scoreTitle">
+              SCORE
+            </div>
+            <div class="score">
+              1234567890
+            </div>
+            <div class="timeTitle">
+              TIME
+            </div>
+            <div class="time">
+              00:00:00
+            </div>
+            <div class="linesTitle">
+              LINE:
+            </div>
+            <div class="lines">
+              10
+            </div>
+            <div class="levalTitle">
+              LEVEL:
+            </div>
+            <div class="level">
+              00
+            </div>
+            <div class="goalTitle">
+              GOAL:
+            </div>
+            <div class="tetrisesTitle">
+              TETRISES:
+            </div>
+            <div class="tetrises">
+              00
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="matrix">
+            <table>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+              <tr>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+                <td>あ</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div class="right">
+          <div class="queue nextQueue">
+            next
+          </div>
+          <div class="queue afterQueue">
+            afterQueue
+          </div>
+        </div>
+      </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.display {
+  height: 100vh;
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #B2B3B2;
+}
+
+.queue {
+  width: 100px;
+  height: 100px;
+  background-color: white;
+}
+</style>
